ac94bec992b14e02e49f2f67d6fb19562dc53b26
_roomi <roomiisdead@gmail.com>
2025-07-08 18:54:25 +0300

    practice

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,63 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+// use rand::Rng;
+// use std::cmp::Ordering;
+// use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    // println!("Guess the number!");
 
-    // --generate a random number--
-    let secret_number = rand::thread_rng().gen_range(1..100);
+    // // --generate a random number--
+    // let secret_number = rand::thread_rng().gen_range(1..100);
 
-    println!("The secret number is {}", secret_number);
+    // println!("The secret number is {}", secret_number);
 
-    // --loop over the comparison stage for multiple tries--
-    loop {
-        println!("Please input your guess.");
+    // // --loop over the comparison stage for multiple tries--
+    // loop {
+    //     println!("Please input your guess.");
 
-        let mut guess = String::new();
+    //     let mut guess = String::new();
 
-        // --get user input for their guess--
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+    //     // --get user input for their guess--
+    //     io::stdin()
+    //         .read_line(&mut guess)
+    //         .expect("Failed to read line");
 
-        // --input is taken as string by default, parsing it into an integer
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+    //     // --input is taken as string by default, parsing it into an integer
+    //     let guess: u32 = match guess.trim().parse() {
+    //         Ok(num) => num,
+    //         Err(_) => continue,
+    //     };
 
-        println!("you guessed {}", guess);
+    //     println!("you guessed {}", guess);
 
-        // --match has arms to compare Less, Greater, and Equal to the value--
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
+    //     // --match has arms to compare Less, Greater, and Equal to the value--
+    //     match guess.cmp(&secret_number) {
+    //         Ordering::Less => println!("Too small!"),
+    //         Ordering::Greater => println!("Too big!"),
+    //         Ordering::Equal => {
+    //             println!("You win!");
+    //             break;
+    //         }
+    //     }
+    // }
+
+    let mut a: [i16; 10] = [1, 2, 3, 4, 5, 6, 1, 10, 22, 1];
+    a.sort();
+
+    for nums in a {
+        is_even(nums);
+        is_odd(nums);
+    }
+}
+
+fn is_even(number: i16) {
+    if number % 2 == 0 {
+        println!("{number} is even");
+        let mut even_nums: [i16; 10] = [];
+    }
+}
+
+fn is_odd(number: i16) {
+    if number % 2 != 0 {
+        println!("{number} is odd")
     }
 }
